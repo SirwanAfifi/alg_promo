b8227de021ea2de60b66d7f4cd8bcb92d09f6364
Sirwan <sir1afifi@gmail.com>
2021-06-20 02:42:02 +0430

    Add CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const shouldSeedDB = process.argv.length > 2 && process.argv[2] === 'seed';
   if (shouldSeedDB) seeder(app).then(() => console.log('Done with Seeding'));
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();

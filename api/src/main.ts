f6dcdd27db877c60f6125f5741333e02692e4385
Sirwan <sir1afifi@gmail.com>
2021-06-19 13:36:12 +0430

    Add seeder

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { seeder } from './utils/seeder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const shouldSeedDB = process.argv.length > 2 && process.argv[2] === 'seed';
+  if (shouldSeedDB) seeder(app).then(() => console.log('Done with Seeding'));
   await app.listen(3000);
 }
 bootstrap();
---
+++ api/src/utils/seeder.ts
@@ -0,0 +1,35 @@
+import { INestApplication } from '@nestjs/common';
+import * as faker from 'faker';
+import { Service } from 'src/entities/_index';
+import { PromoCodeService, ServiceService } from 'src/services/_index';
+
+const getRange = (length: number) =>
+  Array.from({ length }, (_, index) => index);
+
+export const seeder = async (app: INestApplication) => {
+  const service = app.get<ServiceService>('ServiceService');
+  const promoCodeService = app.get<PromoCodeService>('PromoCodeService');
+
+  for (const _ of getRange(100)) {
+    const savedService = await service.createService({
+      title: faker.company.companyName(),
+      price: +faker.commerce.price(),
+      description: faker.commerce.productDescription(),
+    });
+
+    if (!savedService.ok) {
+      continue;
+    }
+
+    const savedServiceData = savedService.data as Service;
+    for (const _ of getRange(10)) {
+      await promoCodeService.createPromoCode({
+        description: faker.commerce.productDescription(),
+        serviceId: savedServiceData.id,
+        percentage: faker.datatype.number({ min: 5, max: 30 }),
+        activeFrom: faker.date.recent(),
+        activeTo: faker.date.between(faker.date.recent(), '2021-12-12'),
+      });
+    }
+  }
+};

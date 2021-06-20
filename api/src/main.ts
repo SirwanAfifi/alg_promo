472deb3f55c90883dd0f371f95ca64106c497e7e
Sirwan <sir1afifi@gmail.com>
2021-06-20 17:03:51 +0430

    Add instruction

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,10 @@ import { seeder } from './utils/seeder';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const shouldSeedDB = process.argv.length > 2 && process.argv[2] === 'seed';
-  if (shouldSeedDB) seeder(app).then(() => console.log('Done with Seeding'));
+  if (shouldSeedDB)
+    seeder(app).then(() => {
+      console.log('Done with Seeding');
+    });
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
